c19a3f8ef59b8c23f0dceb5c4d3b93d737ba33e5
lksmartins <lksmartins11@gmail.com>
2022-10-11 11:49:58 -0300

    SearchBar hidden

--- components/Navbar/SearchBar.js
+++ components/Navbar/SearchBar.js
@@ -2,6 +2,9 @@ import React from 'react'
 import styles from './styles/searchBar.module.css'
 
 export default function searchBar() {
+
+    return null
+
     return(
         <div className={styles.searchBar}>
             

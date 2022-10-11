d2fb7ea80d5a8a8a129419f3930dc8a3a921feb5
caio lucena <caio.lcolaco@gmail.com>
2022-10-11 20:22:24 -0300

    Resolvido upload - faltando resto do formulário e anexo com media

--- next.config.js
+++ next.config.js
@@ -10,7 +10,8 @@ const nextConfig = {
 const env = {
   env: {
     ENV: process.env.ENV,
-    NEXT_PUBLIC_RECAPTCHA_KEY: process.env.RECAPTCHA_KEY
+    NEXT_PUBLIC_RECAPTCHA_KEY: process.env.RECAPTCHA_KEY,
+    NEXT_PUBLIC_BACK_URL: process.env.NEXT_PUBLIC_BACK_URL
   }
 }
 

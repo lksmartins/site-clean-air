e1cead11fe36944c9ad2c853a5b3df2b475c925f
lksmartins <lksmartins11@gmail.com>
2022-10-14 23:05:11 -0300

    /home banner update

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Section from '../components/Section/Section'
@@ -26,6 +27,31 @@ export default function Home() {
         {id:'email', name:'E-mail', type:'email', value:''},
     ]
 
+    const [backgroundPosition, setBackgroundPosition] = useState('center -3rem')
+    const adjustBgPos = ()=>{
+
+        const proportion = window.innerWidth/window.innerHeight
+        let isMobile = window.innerHeight > window.innerWidth
+        let newBgPos = 'center -3rem'
+
+        if( isMobile || proportion < 1.48 ){
+            newBgPos = 'center center'
+        }
+
+        if( newBgPos != backgroundPosition ) setBackgroundPosition(newBgPos)
+    }
+
+    useEffect(() => {
+
+        if( window != undefined ){
+            adjustBgPos()
+            window.addEventListener('resize',adjustBgPos)
+        }
+
+        return () => window.removeEventListener("resize", adjustBgPos);
+
+    },[])
+
     return (
         <>
             <Head>
@@ -35,7 +61,62 @@ export default function Home() {
 
             <main>
 
-                <BannerSection/>
+                <div id="banner"
+                    className="container-fluid d-flex flex-column justify-content-center align-items-center"
+                    style={{
+                        marginTop:          '-85px',
+                        overflow:           'hidden', 
+                        padding:            '10vw 0', 
+                        minHeight:          '20vw', 
+                        maxHeight:          '500px', 
+                        position:           'relative', 
+                        backgroundImage:    'url(/sobre-nos/banner.png)', 
+                        backgroundPosition: backgroundPosition,
+                        backgroundSize:     'cover'
+                    }}
+                    >
+
+                    <div className="container">
+
+                        <div className="d-flex">
+                            <div 
+                                className="text-light fw-bold fs-2"
+                                style={{
+                                    marginTop: '3rem',
+                                }}>
+                                <div>
+                                    <div className="mb-2" style={{
+                                        marginRight: '3rem',
+                                        marginLeft: '-50vw',
+                                        padding: '.5rem 3rem .5rem 50vw',
+                                        boxShadow:'10px 10px 5px -6px rgba(0,0,0,0.75)', 
+                                        backgroundColor:'rgba(0,30,96,0.85)',
+                                        fontWeight: '900',
+                                        lineHeight: '2.5rem'
+                                    }}>
+                                        Qualificação e<br/>
+                                        comprometimento
+                                    </div>
+                                    
+                                    <div className="fs-5 py-3 px-4" style={{
+                                        boxShadow:'10px 10px 5px -6px rgba(0,0,0,0.75)', 
+                                        width:'clamp(320px, 25vw, 960px)', 
+                                        backgroundColor:'rgba(0,163,224,0.75)',
+                                        fontWeight: '200',
+                                        lineHeight: '1.7rem',
+                                    }}>
+                                        Atuando desde 2010 na área de 
+                                        refrigeração, a <b style={{fontWeight: 'bold'}}>Clean Air</b> busca
+                                        garantir a perfeita operação dos
+                                        sistemas de ar condicionado
+                                        visando a satisfação do cliente.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+                </div>
 
                 <Section id="bubbles" className={`${sectionStyles.grid} ${sectionStyles.col4} ${sectionStyles.gap4}`}>
                     {BubbleContent.map(item=>{
